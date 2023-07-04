806ceb1b574893aece4aac862fe589b6c0c1aecb
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2023-07-04 20:56:11 +0900

    PyCharmで実行化

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from enum import Enum
 from typing import Union
-from pydantic import BaseModel
-from fastapi import FastAPI
 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI",
-           "name": "Yusuke", 
-           "age": 26,
-           "device": [
-               "iPhone", "MacBook Air", "AppleWatch"
-           ]}
+            "name": "Yusuke",
+            "age": 26,
+            "device": [
+                "iPhone", "MacBook Air", "AppleWatch"
+            ]}
+
 
 @app.get("/items/{age}")
 async def read_age(age: int):
     return {"message": "Hello FastAPI",
-           "name": "Yusuke", 
-           "age": {age},
-           }
+            "name": "Yusuke",
+            "age": {age},
+            }
 
 
 class Fruits(str, Enum):
@@ -28,24 +30,27 @@ class Fruits(str, Enum):
     banana = "Banana"
     peach = "Peach"
 
+
 @app.get("/fruits/{fruits}")
 async def get_fruits(fruits: Fruits):
-    dict = {"message": "OK", "fruits":""}
+    dic = {"message": "OK", "fruits": ""}
 
     if fruits is Fruits.apple:
-        dict["fruits"] = Fruits.apple
+        dic["fruits"] = Fruits.apple
     elif fruits is Fruits.banana:
-        dict["fruits"] = Fruits.banana
+        dic["fruits"] = Fruits.banana
     elif fruits is Fruits.peach:
-        dict["fruits"] = Fruits.peach
-    
-    return dict
+        dic["fruits"] = Fruits.peach
+
+    return dic
+
 
 db_items = [
     {"item_name": "Foo"},
     {"item_name": "Bar"},
     {"item_name": "Buzz"},
 ]
+
 
 @app.get("/search/{item_no}")
 async def get_search(item_no: int, q: Union[str, None] = None):
@@ -56,8 +61,9 @@ async def get_search(item_no: int, q: Union[str, None] = None):
 
 class Account(BaseModel):
     name: str
-    discription: Union[str, None] = None
+    description: Union[str, None] = None
     age: int
+
 
 @app.post("/account/")
 async def account(account: Account):
